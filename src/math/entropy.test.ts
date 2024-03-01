806bf35a9eb1f8e3155b4bb98d82c4a49c3ed556
drag13 <no-questions@github.noreply.com>
2024-03-01 12:18:31 +0200

    More tests for entropy

--- src/math/entropy.test.ts
+++ src/math/entropy.test.ts
@@ -1,6 +1,32 @@
 import { calculateEntropy } from "./entropy";
 
-test(`For the same values, entropy should be 0`, ()=> {
-  const DATA = ["1" , "1", "1"];
+test(`For the same values, entropy should be 0`, () => {
+  const DATA = ["1", "1", "1"];
   expect(calculateEntropy(DATA, DATA[0])).toBe(0);
-})
+});
+
+test(`For the 1,1,2,2 values, entropy should be 1`, () => {
+  const DATA = ["1", "1", "2", "2"];
+  expect(calculateEntropy(DATA, DATA[0])).toBe(1);
+});
+
+test(`For the given values, entropy should be "0.94`, () => {
+  const DATA = [
+    "NO",
+    "NO",
+    "YES",
+    "YES",
+    "YES",
+    "NO",
+    "YES",
+    "NO",
+    "YES",
+    "YES",
+    "YES",
+    "YES",
+    "YES",
+    "NO",
+  ];
+  const columnEntropy = calculateEntropy(DATA, DATA[0]);
+  expect(columnEntropy.toFixed(2)).toBe("0.94");
+});
--- src/math/entropy.ts
+++ src/math/entropy.ts
@@ -1,23 +1,25 @@
-export function calculateEntropy (dataSet: string[], value: string) {
-    if (!dataSet.length) {
-      return 0;
-    }
-    const allLabelsLength = dataSet.length;
-    const givenLabelLength = dataSet.filter((x) => x === value).length;
-  
-    if (!givenLabelLength) {
-      throw new Error(`VALUE "${value}" not found in the dataset`);
-    }
-  
-    if (givenLabelLength === dataSet.length) {
-      return 0;
-    }
-  
-    const otherLabelsQuantity = allLabelsLength - givenLabelLength;
-    const valueEntropy =
-      (givenLabelLength / allLabelsLength) * Math.log2(givenLabelLength / allLabelsLength);
-    const otherEntropy =
-      (otherLabelsQuantity / allLabelsLength) * Math.log2(otherLabelsQuantity / allLabelsLength);
-  
-    return -1 * (valueEntropy + otherEntropy);
+export function calculateEntropy(dataSet: string[], value: string) {
+  if (!dataSet.length) {
+    return 0;
   }
+  const allLabelsLength = dataSet.length;
+  const givenLabelLength = dataSet.filter((x) => x === value).length;
+
+  if (!givenLabelLength) {
+    throw new Error(`VALUE "${value}" not found in the dataset`);
+  }
+
+  if (givenLabelLength === dataSet.length) {
+    return 0;
+  }
+
+  const otherLabelsQuantity = allLabelsLength - givenLabelLength;
+  const valueEntropy =
+    (givenLabelLength / allLabelsLength) *
+    Math.log2(givenLabelLength / allLabelsLength);
+  const otherEntropy =
+    (otherLabelsQuantity / allLabelsLength) *
+    Math.log2(otherLabelsQuantity / allLabelsLength);
+
+  return -1 * (valueEntropy + otherEntropy);
+}
